466fac66533d3a239246a1cadb39524384f6d558
prestwia <prestwia@oregonstate.edu>
2022-11-01 11:43:12 -0700

    hello window OOP

--- musicai/visualization/score_window.py
+++ musicai/visualization/score_window.py
@@ -1,18 +1,26 @@
 import pyglet
 
-window = pyglet.window.Window()
 
-label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+class ScoreWindow(pyglet.window.Window):
+    def __init__(self):
+        super(ScoreWindow, self).__init__()
+
+        self.label = pyglet.text.Label('Hello, world',
+                                       font_name='Times New Roman',
+                                       font_size=36,
+                                       x=self.width//2, y=self.height//2,
+                                       anchor_x='center', anchor_y='center')
+
+    def on_draw(self):
+        self.clear()
+        self.label.draw()
 
 
-@window.event
-def on_draw():
-    window.clear()
-    label.draw()
+def main():
+    """Main function"""
+    window = ScoreWindow()
+    pyglet.app.run()
 
 
-pyglet.app.run()
+if __name__ == "__main__":
+    main()
