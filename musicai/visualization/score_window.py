a7dcf1267366a200f0c3dce16148c59abe42a203
prestwia <prestwia@oregonstate.edu>
2022-11-03 16:07:26 -0700

    Bravura font data, label prints unicode glyph.

--- musicai/visualization/score_window.py
+++ musicai/visualization/score_window.py
@@ -5,14 +5,22 @@ class ScoreWindow(pyglet.window.Window):
     def __init__(self):
         super(ScoreWindow, self).__init__()
 
-        self.label = pyglet.text.Label('Hello, world',
-                                       font_name='Times New Roman',
+        self.background = pyglet.image.SolidColorImagePattern(
+            (255, 255, 255, 255)).create_image(self.width, self.height)
+
+        pyglet.font.add_file('./assets/Bravura.otf')
+        bravura = pyglet.font.load('Bravura')
+
+        self.label = pyglet.text.Label(chr(int('F472', 16)),
+                                       font_name='Bravura',
                                        font_size=36,
                                        x=self.width//2, y=self.height//2,
                                        anchor_x='center', anchor_y='center')
+        self.label.color = (0, 0, 0, 255)
 
     def on_draw(self):
         self.clear()
+        self.background.blit(0, 0)
         self.label.draw()
 
 
