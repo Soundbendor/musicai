adbcae2370df3d5ee125937bc654d99450614ec6
Paul Koos <paully_k5298@yahoo.com>
2023-02-28 10:05:26 -0800

    convert WindowConfig to use the singleton pattern. There shouldnt be any need to have more than one of this specific class in the runtime.

--- musicai/visualization/window_config.py
+++ musicai/visualization/window_config.py
@@ -1,23 +1,17 @@
 from dotenv import dotenv_values
 
-_DEBUG = False
 
+class WindowConfig(object):
+    _instance = None
 
-class WindowConfig:
-    def __init__(self, file_name: str = ".msvconfig"):
-        if _DEBUG:
-            print(f"WindowConfig __init__")
+    def __new__(cls, file_name: str = ".msvconfig"):
+        if cls._instance is None:
+            cls._instance = super(WindowConfig, cls).__new__(cls)
+            cls._instance._parse_config_file(dotenv_values(file_name))
+        return cls._instance
 
-        self.file_name = file_name
-        self.data = dotenv_values(self.file_name)
-        self._parse_config_file()
-
-    def _parse_config_file(self):
-        if _DEBUG:
-            print("WindowConfig::parse_config_file")
-        for key, value in self.data.items():
-            if _DEBUG:
-                print(f"{key}: {value}")
+    def _parse_config_file(self, config_data):
+        for key, value in config_data.items():
             try:
                 a_val = int(value)
             except ValueError:
