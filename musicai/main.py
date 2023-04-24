8c4c372fc42a152e9b41f61575f6e37054e27c72
cheneak <cheneak@oregonstate.edu>
2023-04-24 13:56:03 -0700

    added barlines test score

--- musicai/main.py
+++ musicai/main.py
@@ -10,10 +10,11 @@ def main():
     print('main module')
 
     # ---- Test Scores ----
-    # file = '../examples/score_tests/notes.musicxml'
-    # file = '../examples/score_tests/accidentals.musicxml'
+    #file = '../examples/score_tests/notes.musicxml'
+    #file = '../examples/score_tests/accidentals.musicxml'
     #file = '../examples/score_tests/accents.musicxml'
     #file = '../examples/score_tests/dynamics.musicxml'
+    #file = '../examples/score_tests/barlines.musicxml'
 
     # ---- Decent looking ./mxml scores ----
     file = '../examples/mxml/MozartTrio.musicxml'
